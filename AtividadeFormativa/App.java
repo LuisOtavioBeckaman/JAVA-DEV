9dadf92118f9520707922914da963295fd409d38
Luis Otávio Beckman <123770533+LuisOtavioBeckaman@users.noreply.github.com>
2023-05-23 17:48:33 +0000

    Receba

---
+++ AtividadeFormativa/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        AtividadeFormativa obj = new AtividadeFormativa();
+            obj.atividade1();
+            obj.atividade2();
+            obj.atividade3();
+        }
+    }
---
+++ AtividadeFormativa/AtividadeFormativa.java
@@ -0,0 +1,114 @@
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class AtividadeFormativa {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+
+    public void atividade1() {
+
+        System.out.println("Por favor dígite o número de colunas desejadas:");
+        int col = sc.nextInt();
+        System.out.println("Por favor dígite o número de Linhas desejadas");
+        int row = sc.nextInt();
+
+        int tab[][] = new int[col][row];
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                tab[i][j] = rd.nextInt(9);
+            }
+        }
+        for (int i = 0; i < col; i++) {
+            System.out.print(" |  ");
+            for (int j = 0; j < row; j++) {
+                System.out.print(+tab[i][j] + " ");
+            }
+            System.out.println(" | ");
+        }
+        System.out.println("==================");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                tab[i][j] = rd.nextInt(9);
+                if (i > j) {
+                    tab[i][j] = 1;
+                }
+                if (i == j) {
+                    tab[i][j] = 0;
+                }
+                if (i < j) {
+                    tab[i][j] = 2;
+                }
+
+            }
+        }
+        for (int i = 0; i < col; i++) {
+            System.out.print(" | ");
+            for (int j = 0; j < row; j++) {
+                System.out.print(+tab[i][j] + " ");
+            }
+            System.out.println(" | ");
+
+        }
+    }
+
+    public void atividade2() {
+        Random rd = new Random();
+        int cont = 1;
+        int valorSorteado = rd.nextInt(1000);
+        boolean tenteNovamente = true;
+        while (tenteNovamente) {
+            System.out.println("Digite um Nº");
+            int nDigitado = sc.nextInt();
+            if (nDigitado == valorSorteado) {
+                System.out.println("Acertouuuuuuuuuuuuu!!!!!!");
+                System.out.println("Números de tentativas: " + cont);
+                tenteNovamente = false;
+            } else if (nDigitado > valorSorteado) {
+                System.out.println("O valor sorteado é menor do que o número digitado");
+                cont++;
+            } else if (nDigitado < valorSorteado) {
+                System.out.println("O valor sorteado é maior que o número digitado");
+                cont++;
+            }
+        }
+    }
+
+    public void atividade3() {
+            
+                Random random = new Random();
+
+                // Sorteia o tamanho entre 100 e 1000
+                int tamanhoVetor = random.nextInt(901) + 100; 
+
+                 int[] vetor = new int[tamanhoVetor];
+            
+                // Preenche o vetor com números aleatórios de 1 a 100
+                for (int i = 0; i < tamanhoVetor; i++) {
+                        vetor[i] = random.nextInt(100) + 1;
+                    }
+                    
+                for (int i = 0; i < vetor.length; i++) {
+                    if (vetor[i]%2 == 0){
+                        System.out.println("O vetor["+i+"] = "+ vetor[i]);
+                    }
+                 
+                }
+                for (int i = 0; i < vetor.length; i++) {
+                    if (vetor[i]%2 == 1){
+                        System.out.println("O vetor["+i+"] = "+ vetor[i]);
+                    }
+                }
+                for (int i = 0; i < vetor.length; i++) {
+                    if (vetor[i]%2 == 0 && (i+1)%2 == 1){
+                        System.out.println("O vetor da posição"  + (i + 1)+ "=" +vetor[i]);
+                    }
+                }
+                for (int i = 0; i < vetor.length; i++) {
+                    if (vetor[i]%2 == 1 && (i+1)%2 == 0){
+                        System.out.println("O vetor da posição"  + (i + 1)+ "=" +vetor[i]);
+                    }
+                }
+            }
+        }
